a490008d106501ec2fca3bbd5840f0fce44c3cb6
Facundo Aguilar <facundoaguilar96@hotmail.com>
2021-11-19 02:15:54 -0300

    contact quedo con fondo body

--- clear.js
+++ clear.js
@@ -1,8 +1,5 @@
 function main() {
-  const backgroundClearEl = document.querySelector(".contact-content");
-  console.log(backgroundClearEl);
   const clear = document.querySelector(".contact");
-  console.log(clear);
-  clear.style.removeProperty("background-color");
+  clear.style.backgroundColor = "transparent";
 }
 main();
